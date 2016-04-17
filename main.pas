962974cc721bc98773bfc889484896f53bc5cc40
M Fathoni Nurrohman <muh.fathoni77@yahoo.co.id>
2016-04-17 21:20:11 +0700

    Added files via upload

--- main.pas
+++ main.pas
@@ -1,9 +1,13 @@
 program ReservasiTiketBioskop;
-uses uFilm, 
+uses uFilm, uJadwal, uKapasitas, uMember, uTanggal,
 		sysutils,crt;
 
 var
-	tFilm : dFilm;
+	tFilm	: dbFilm;
+	tTayang	: dbTayang;
+	tKapasitas: dbKapasitas;
+	tMember : dbMember;
+	tgl : Tanggal;
 	Fx : string;
 	stopProgram : boolean;
 	
@@ -18,16 +22,20 @@ begin
 		writeln('===========================================================');
 		writeln();
 		writeln('Berikut adalah Fungsi yang bisa digunakan:                 ');
-		writeln('  1. load            6. ratingFilter   11. payCreditCard   ');
-		writeln('  2. nowPlaying      7. searchMovie    12. payMember       ');
-		writeln('  3. upcoming        8. showMovie      13. loginMember     ');
-		writeln('  4. schedule        9. showNextDay    14. register        ');
-		writeln('  5. genreFilter    10. selectMovie    15. exit            ');
+		writeln(' 1. load            6. ratingFilter   11. payCreditCard    ');
+		writeln(' 2. nowPlaying      7. searchMovie    12. payMember        ');
+		writeln(' 3. upcoming        8. showMovie      13. loginMember      ');
+		writeln(' 4. schedule        9. showNextDay    14. register         ');
+		writeln(' 5. genreFilter    10. selectMovie    15. exit             ');
 		writeln();
 		write('> ');readln(Fx);
 		case Fx of
 			'load'			: begin
 								loadFilm(tFilm);
+								loadTayang(tTayang);
+								loadKapasitas(tKapasitas);
+								loadMember(tMember);
+								loadTanggal(tgl);
 								delay(1500);
 							  end;
 			'genreFilter'	: genreFilter(tFilm);
